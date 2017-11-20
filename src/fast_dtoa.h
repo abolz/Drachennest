abe54ce22b891addb564536625716019c10c590e
abolz <lt.morris.schaffer@googlemail.com>
2017-11-20 09:49:21 +0100

    Add license

--- src/fast_dtoa.h
+++ src/fast_dtoa.h
@@ -1,7 +1,30 @@
+// Copyright 2017 Alexander Bolz
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 // Implements the Grisu2 algorithm for binary to decimal floating-point
 // conversion.
-// The implementation is based on the reference implementation. The original
-// licence can be found at the end of this file.
+//
+// This implementation is a slightly modified version of the reference
+// implementation by Florian Loitsch which can be obtained from
+// http://florian.loitsch.com/publications (bench.tar.gz)
+// The original license can be found at the end of this file.
 //
 // References:
 //
