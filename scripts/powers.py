d885c37206ec53921863969e3519c138e3d60201
Alexander Bolz <lt.morris.schaffer@googlemail.com>
2019-04-28 12:24:38 +0200

    Clean up

--- scripts/powers.py
+++ scripts/powers.py
@@ -2,17 +2,32 @@
 #
 #===============================================================================
 
-def FormatHexChunks(n, bits_per_chunk = 64):
+# def Sum(chunks, bits_per_chunk):
+#     assert bits_per_chunk > 0
+#     return sum( map(lambda (i, c): c * (2**bits_per_chunk)**i, enumerate(chunks)) )
+
+def Split(n, bits_per_chunk):
+    assert n >= 0
     assert bits_per_chunk > 0
-    assert bits_per_chunk % 4 == 0
-    q, r = divmod(n, 2**bits_per_chunk)
-    s = '0x{:0{}X}'.format(r, bits_per_chunk/4)
-    chunks = 1
-    while q != 0:
-        q, r = divmod(q, 2**bits_per_chunk)
-        s = '0x{:0{}X}, '.format(r, bits_per_chunk/4) + s
-        chunks += 1
-    if chunks > 1:
+    chunks = []
+    while True:
+        n, r = divmod(n, 2**bits_per_chunk)
+        chunks.append(r)
+        if n == 0:
+            break
+    return chunks
+
+def ToHexString(n, bits):
+    assert bits > 0
+    p = (bits + (4 - 1)) // 4       # Round up to four bits per hexit
+#   p = 2**((p - 1).bit_length())   # Round up to next power-of-2
+    assert 4*p >= n.bit_length()
+    return '0x{:0{}X}'.format(n, p)
+
+def FormatHexChunks(n, bits_per_chunk = 64):
+    chunks = Split(n, bits_per_chunk)
+    s = ', '.join(map(lambda x: ToHexString(x, bits_per_chunk), reversed(chunks)))
+    if len(chunks) > 1:
         s = '{' + s + '}'
     return s
 
@@ -21,15 +36,20 @@ def FormatHexChunks(n, bits_per_chunk = 64):
 #===============================================================================
 
 def FloorLog2Pow10(e):
+    assert e >= -1233
+    assert e <= 1232
     return (e * 1741647) // 2**19
 
 def RoundUp(num, den):
+    assert num >= 0
+    assert den > 0
     p, r = divmod(num, den)
     if 2 * r >= den:
         p += 1
     return p
 
 def ComputeGrisuPower(k, bits):
+    assert bits > 0
     e = FloorLog2Pow10(k) + 1 - bits
     if k >= 0:
         if e > 0:
@@ -61,15 +81,20 @@ def PrintGrisuPowers(bits, min_exponent, max_exponent, step = 1):
 #===============================================================================
 
 def FloorLog2Pow5(e):
+    assert e >= -1764
+    assert e <= 1763
     return (e * 1217359) // 2**19
 
 def Ceil(num, den):
+    assert num >= 0
+    assert den > 0
     q, r = divmod(num, den)
     if r > 0:
         q += 1
     return q
 
 def ComputeRyuPower(k, bits):
+    assert bits > 0
     e = FloorLog2Pow5(k) + 1 - bits
     if k >= 0:
         if e > 0:
@@ -91,7 +116,7 @@ def PrintRyuPowers(bits, min_exponent, max_exponent):
         print FormatHexChunks(f, bits_per_chunk=64) + ', // e = {:5d}, k = {:4d}'.format(e, k)
 
 # For double-precision:
-PrintRyuPowers(bits=128, min_exponent=-291, max_exponent=325)
+# PrintRyuPowers(bits=128, min_exponent=-291, max_exponent=325)
 
 # For single-precision:
-# PrintRyuPowers(bits=64, min_exponent=-30, max_exponent=47)
+PrintRyuPowers(bits=64, min_exponent=-30, max_exponent=47)
