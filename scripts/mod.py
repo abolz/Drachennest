1e1c881a4e13609ed787f1b85acac3281467f8eb
Alexander Bolz <lt.morris.schaffer@googlemail.com>
2019-06-18 19:20:40 +0200

    Use python 3

--- scripts/mod.py
+++ scripts/mod.py
@@ -14,13 +14,13 @@ def ModularInverse(a, m):
 
 def PrintTable(max_e5, bits):
     assert bits % 4 == 0
-    print '    {{0x{:0{}X}u, 0x{:0{}X}u}},'.format(1, bits // 4, 2**bits - 1, bits // 4)
+    print('    {{0x{:0{}X}u, 0x{:0{}X}u}},'.format(1, bits // 4, 2**bits - 1, bits // 4))
     for e in range(1, max_e5 + 1):
         a = 5**e
         m = ModularInverse(a, 2**bits)
         assert (a * m) % 2**bits == 1
-        print '    {{0x{:0{}X}u, 0x{:0{}X}u}},'.format(m, bits // 4, 2**bits // a, bits // 4)
-    print ''
+        print('    {{0x{:0{}X}u, 0x{:0{}X}u}},'.format(m, bits // 4, 2**bits // a, bits // 4))
+    print('')
 
 PrintTable(max_e5=10, bits=32) # 10 = floor(log_5(2^26))
 PrintTable(max_e5=22, bits=64) # 22 = floor(log_5(2^55))
